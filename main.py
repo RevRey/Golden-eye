9ba25fff6ba697ecb3bd436fab49f7918e7e2349
Revanthravi <revanthravi321@gmail.com>
2025-01-30 00:03:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-class carr:
+class car:
   def __init__(self,model:str,year:int):
     self.model=model
     self.year=year
   def auto(self):
     return f'the {self.model} is {self.year}' 
 
-car1 = carr("sedan",1999)
+car1 = car("sedan",1999)
 car1.auto()
